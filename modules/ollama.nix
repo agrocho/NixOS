4d84a0c6e3eb266836b028f953a4fdc42bb81673
Agro <134230906+agrocho@users.noreply.github.com>
2026-06-25 15:02:48 -0400

    Update ollama.nix

--- modules/ollama.nix
+++ modules/ollama.nix
@@ -1,34 +1,22 @@
-# This file contains settings for Ollama and Open-WebUI.
-
-{ config, pkgs, ...}:
+{ config, pkgs, ... }:
 
 {
+  services.ollama = {
+    enable = true;
+    package = pkgs.ollama-vulkan;
+    host = "127.0.0.1";
+  };
 
-# Ollama Configuration
-
-	services.ollama = {
-		enable = true;
-		package = pkgs.ollama-vulkan;
-	};
-
-	systemd.services.ollama.serviceConfig = {
-		environment = [ "OLLAMA_HOST=0.0.0.0:11434" ];
-	};
-
-# Open-WebUI
-
-	services.open-webui = {
-		enable = true;
-		environment = {
-			ANONYMIZED_TELEMETRY = "False";
-			DO_NOT_TRACK = "True";
-			SCARF_NO_ANALYTICS = "True";
-			OLLAMA_API_BASE_URL = "http://127.0.0.1:11434/api";
-			OLLAMA_BASE_URL = "http://127.0.0.1:11434";
-		};
-		port = 8080;
-	};
-
-
-
+  services.open-webui = {
+    enable = true;
+    host = "127.0.0.1";
+    port = 8080;
+    environment = {
+      ANONYMIZED_TELEMETRY = "False";
+      DO_NOT_TRACK = "True";
+      SCARF_NO_ANALYTICS = "True";
+      OLLAMA_API_BASE_URL = "http://127.0.0.1:11434/api";
+      OLLAMA_BASE_URL = "http://127.0.0.1:11434";
+    };
+  };
 }
